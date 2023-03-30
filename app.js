0f7f26351d8dcc71a1d4cf864914ec5fb00632a8
weimeng <xu13071061935@163.com>
2023-03-30 17:40:44 +0800

    修改koa-static

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+const path = require('path')
 const Koa = require('koa');
 const static = require('koa-static')
 
 
 const app = new Koa();
-app.use(static(__dirname, 'build'))
+app.use(static(path.resolve('build')))
 
 app.use(async (ctx) => {
 
 })
 
 app.listen(3389, () => {
-	console.log('marry app is running at 3000')
+	console.log('marry app is running at 3389')
 })
